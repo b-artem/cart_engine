84a3e13c77c24f51ed249417d7ae9d9cfb25d5cf
b-artem <artemb.dev@gmail.com>
2017-11-16 17:45:16 +0200

    fix migrations

--- db/migrate/20171115125636_add_coupon_id_to_cart_orders.rb
+++ db/migrate/20171115125636_add_coupon_id_to_cart_orders.rb
@@ -1,6 +1,6 @@
 class AddCouponIdToCartOrders < ActiveRecord::Migration[5.1]
   def change
     add_column :cart_orders, :coupon_id, :integer
+    add_index :cart_orders, :coupon_id
   end
-  add_index :cart_orders, :coupon_id
 end
--- db/migrate/20171115125726_add_coupon_id_to_cart_carts.rb
+++ db/migrate/20171115125726_add_coupon_id_to_cart_carts.rb
@@ -1,6 +1,6 @@
 class AddCouponIdToCartCarts < ActiveRecord::Migration[5.1]
   def change
     add_column :cart_carts, :coupon_id, :integer
+    add_index :cart_carts, :coupon_id
   end
-  add_index :cart_carts, :coupon_id
 end
