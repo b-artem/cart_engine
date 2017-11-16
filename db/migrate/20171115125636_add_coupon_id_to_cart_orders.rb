9805dd11671360d2dce8f0fc25efdc3b9bdec3f7
b-artem <artemb.dev@gmail.com>
2017-11-16 17:14:52 +0200

    fix specs

--- db/migrate/20171115125636_add_coupon_id_to_cart_orders.rb
+++ db/migrate/20171115125636_add_coupon_id_to_cart_orders.rb
@@ -1,6 +1,6 @@
 class AddCouponIdToCartOrders < ActiveRecord::Migration[5.1]
   def change
-    add_column :cart_orders, :coupon_id, :integer, foreign_key: true
+    add_column :cart_orders, :coupon_id, :integer
   end
   add_index :cart_orders, :coupon_id
 end
